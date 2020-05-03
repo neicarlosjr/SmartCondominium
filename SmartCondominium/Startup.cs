0bc777d2159e4ecf895c113a4a870da02d1141f1
neica <neica@DESKTOP-NCJ>
2020-05-03 12:07:55 -0300

    Add project files.

---
+++ SmartCondominium/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartCondominium.Startup))]
+namespace SmartCondominium
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
